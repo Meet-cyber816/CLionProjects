82914bc370a9338a475fe8224af0fba1b76d4932
Meet Panchal <meet24086@gmail.com>
2025-08-06 19:05:45 +0530

    Update Polynomial

--- DSA/Polynomial/main.cpp
+++ DSA/Polynomial/main.cpp
@@ -7,10 +7,11 @@ struct Poly {
     int coeff;
 };
 
-void add(const vector<Poly> &p4,const vector<Poly> &p5, vector<Poly> &p3) {
+typedef vector<Poly> Parray;
+void add(const Parray &p4,const Parray &p5, Parray &p3) {
     int i=0,j=0,k=0;
     while (i<p4.size() && j<p5.size()) {
-        if (p4[i].exp == p5[i].exp) {
+        if (p4[i].exp == p5[j].exp) {
             p3.push_back( {p4[i].exp, p4[i].coeff + p5[j].coeff} );
             //p3[k].exp = p4[i].exp;
             i++; j++;
@@ -37,26 +38,32 @@ void add(const vector<Poly> &p4,const vector<Poly> &p5, vector<Poly> &p3) {
         j++;
     }
 }
-void display(vector<Poly> &p3) {
+void display(Parray &p3) {
     for ( auto x : p3) {
-        cout<<x.coeff<<" "<<x.exp<<endl;
+        cout<<x.coeff<<"x^"<<x.exp<<" + ";
     }
+    cout<<" 0 "<<" = "<<" 0 ";
     cout<<endl<<endl;
 }
 
 
 int main() {
-    vector<Poly> p1(3), p2(2),ans;
-    p1[0].coeff = 3;
-    p1[0].exp = 2;
-    p1[1].coeff = 2;
-    p1[1].exp = 1;
-    p1[2].coeff = 3;
-    p1[2].exp = 0;
-    p2[0].coeff = 4;
-    p2[0].exp = 4;
-    p2[1].coeff = 3;
-    p2[1].exp = 1;
+    Parray p1, p2,ans;
+    for (int i=0;i<=5;i++)
+        p1.push_back({5-i,10-i});
+
+    for (int j=1;j<8;j++)
+        p2.push_back({17-2*j, 99-7*j});
+    // p1[0].coeff = 3;
+    // p1[0].exp = 2;
+    // p1[1].coeff = 2;
+    // p1[1].exp = 1;
+    // p1[2].coeff = 3;
+    // p1[2].exp = 0;
+    // p2[0].coeff = 4;
+    // p2[0].exp = 4;
+    // p2[1].coeff = 3;
+    // p2[1].exp = 1;
     display(p1);
     display(p2);
     add(p1,p2,ans);
